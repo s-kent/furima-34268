b4a5726382120eaad471a34b2307520167dfe31b
s-kent <s.kento0210@gmail.com>
2021-05-08 15:59:32 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :products
+  has_many :comments
+  has_many :purchase_managements
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  
+  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: '全角カタカナのみで入力して下さい' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+
+  validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+end
+
+ 
