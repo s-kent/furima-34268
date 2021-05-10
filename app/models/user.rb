f6ad3a81d90797274be8a71fb86acd04c2384c9d
s-kent <s.kento0210@gmail.com>
2021-05-10 13:40:17 +0900

    モデルの単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,20 +11,21 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
     validates :first_name
     validates :last_name
   end
   
-  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: '全角カタカナのみで入力して下さい' } do
+  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ } do
     validates :first_name_kana
     validates :last_name_kana
   end
 
 
   validates :birthday, presence: true
+  
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX
 end
 
  
