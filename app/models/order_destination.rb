c62f097d36a9f9209789471bb9a2fb3d7bb6c98b
s-kent <s.kento0210@gmail.com>
2021-05-22 11:53:57 +0900

    order_destinationモデルの修正

--- app/models/order_destination.rb
+++ app/models/order_destination.rb
@@ -1,6 +1,6 @@
 class OrderDestination
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id,:user_id, :product_id, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :product_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
